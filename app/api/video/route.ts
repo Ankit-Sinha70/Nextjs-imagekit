8a6074ffcb70a7e54128ab0dc3e980a2557591a8
Ankit Sinha <ankits.brainerhub@gmail.com>
2025-06-16 13:46:24 +0530

    Added imagekit code

---
+++ app/api/video/route.ts
@@ -0,0 +1,80 @@
+import { authOptions } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db";
+import Video from "@/models/Video";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { Ivideo } from "../../../models/Video";
+
+export async function GET() {
+  try {
+    await connectToDatabase();
+    const videos = await Video.find({}).sort({ createdAt: -1 }).lean();
+
+    if (!videos || videos.length === 0) {
+      return NextResponse.json([], { status: 200 });
+    }
+    return NextResponse.json(videos);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Failed to fetch videos",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "Unauthorized User",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+    await connectToDatabase();
+    const body: Ivideo = await request.json();
+    if (
+      !body.title ||
+      !body.description ||
+      !body.thumbnailUrl ||
+      !body.videoUrl
+    ) {
+      return NextResponse.json(
+        {
+          error: "Invalid Request",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    const videoData = {
+      ...body,
+      controls: body.controls ?? true,
+      transformation: {
+        height: 1920,
+        width: 1080,
+        quality: body.transformation?.quality ?? 100,
+      },
+    };
+    const newVideo = await Video.create(videoData);
+    return NextResponse.json(newVideo);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Failed to create video",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
